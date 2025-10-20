52e521b70c28be2ebe32dbfa03a638616fa02b2e
Sean James <aseanjames2020@gmail.com>
2025-10-20 19:29:22 +0100

    FT-100-java-spring-initial-setup: Initial commit including Spring Initialzr setup with Maven and Spring boot integrated. Minor changes for naming of App and also add println to java main() and check junit working.. Minor gitignores of project specific environment of Maven and Vscode

---
+++ tdximpact-challenge/src/test/java/com/subzero/tdximpact_challenge/AppMainTest.java
@@ -0,0 +1,19 @@
+package com.subzero.tdximpact_challenge;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class AppMainTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void whenAssertEqualsIsCalledWithTwoNumbersAddedTheCorrectResultWillBeMatched() {
+		//TODO: Test will always pass initially, checking that program compiles and junit is setup OK.
+		assertEquals(2,1+1);
+	}
+
+}
