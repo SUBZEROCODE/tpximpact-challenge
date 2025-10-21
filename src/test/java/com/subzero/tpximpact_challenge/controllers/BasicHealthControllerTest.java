498b89456dbe61f691823dc26362c6629a1d87bd
Sean James <aseanjames2020@gmail.com>
2025-10-21 14:25:42 +0100

    Minor additons of JSONObject to pom.xml (was working as tested previously) and context-path bug will be another bugfix story: BUG-100-api-context-path-fix

--- src/test/java/com/subzero/tpximpact_challenge/controllers/BasicHealthControllerTest.java
+++ src/test/java/com/subzero/tpximpact_challenge/controllers/BasicHealthControllerTest.java
@@ -3,7 +3,6 @@ package com.subzero.tpximpact_challenge.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
