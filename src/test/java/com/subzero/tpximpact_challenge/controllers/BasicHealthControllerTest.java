47753a2a96f205c98111a86020ae0441b98c55ca
Sean James <aseanjames2020@gmail.com>
2025-10-21 12:00:49 +0100

    Add in simple test for a basic health endpoint to prove junit and webmvc mocking of tests working.Note: version of java spring boot now 4.0.0-M1 as "snapshot" did not have webmvctest as required

---
+++ src/test/java/com/subzero/tpximpact_challenge/controllers/BasicHealthControllerTest.java
@@ -0,0 +1,27 @@
+package com.subzero.tpximpact_challenge.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest(BasicHealthController.class)
+@ContextConfiguration(classes = BasicHealthController.class)
+public class BasicHealthControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void healthCheckGetShouldReturnStatusOKAndStringReadyToServeAPI() throws Exception {
+        //TODO: Context needs fixing so that the path is correctly being inherited in tests.
+        mockMvc.perform(get("/api/url-shortener/v1/health"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Java Spring is ready to serve the API"));
+    }
+
+}
