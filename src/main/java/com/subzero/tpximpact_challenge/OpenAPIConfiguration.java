e4418bbaed2544852559a72c7934cdf42019accb
Sean James <aseanjames2020@gmail.com>
2025-10-22 13:14:25 +0100

    Minor addition as openapi-boilerplate code integration working, now to fix the CORS issues.

---
+++ src/main/java/com/subzero/tpximpact_challenge/OpenAPIConfiguration.java
@@ -0,0 +1,26 @@
+package com.subzero.tpximpact_challenge;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+// @Configuration
+// public class OpenAPIConfiguration {
+//     Server server = new Server();
+//     server.setUrl("http://localhost:8080");
+//     server.setDescription("Local server");
+
+//    @Bean
+//    public OpenAPI defineOpenApi() {
+//     Info projectInfo = new Info()
+//             .title("URL Shortener API")
+//             .description("Simple RESTful API for shortening URLs.")
+//             .version("1.0.0");
+//     return new OpenAPI().info(projectInfo).servers(List.of(server));
+//    }
+
+    
+// }
