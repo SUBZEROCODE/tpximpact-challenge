8d335ddf96241a00a1b4d90f408333f7dd5967e2
Sean James <aseanjames2020@gmail.com>
2025-10-24 12:52:57 +0100

    Annotate the java @Entity with @Table to map to the right table, add the challenges of flyway and finally gitignore .db persisted files from commits

--- src/main/java/com/subzero/tpximpact_challenge/models/AliasWithUrlMapping.java
+++ src/main/java/com/subzero/tpximpact_challenge/models/AliasWithUrlMapping.java
@@ -3,8 +3,8 @@ package com.subzero.tpximpact_challenge.models;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +15,7 @@ import lombok.Setter;
 
 @Entity
 @EqualsAndHashCode
+@Table(name = "alias_with_url_mapping")
 public class AliasWithUrlMapping {
 
     @Id
