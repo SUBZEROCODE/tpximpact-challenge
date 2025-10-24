6214daccd085fe47955edcf26afe66dbbd1a8f60
Sean James <aseanjames2020@gmail.com>
2025-10-24 11:13:39 +0100

    Tried using builder, but in the end simple if and else logic seems to be the best way

--- src/test/java/com/subzero/tpximpact_challenge/util/MockAliasUrlMappingBuilder.java
+++ src/test/java/com/subzero/tpximpact_challenge/util/MockAliasUrlMappingBuilder.java
@@ -2,9 +2,6 @@ package com.subzero.tpximpact_challenge.util;
 
 import com.subzero.tpximpact_challenge.models.AliasWithUrlMapping;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-
 public class MockAliasUrlMappingBuilder {
     public static AliasWithUrlMapping getCustomStubbedAliasWithUrlMappingForTesting(String alias,String fullUrl,String shortUrl) {
 
@@ -35,29 +32,3 @@ public class MockAliasUrlMappingBuilder {
     }
 
 }
-
-//     // Private constructor to enforce builder usage
-//     private MockAliasUrlMappingBuilder(Builder builder) {
-//         this.alias = builder.alias;
-//         this.fullUrl = builder.fullUrl;
-//         this.shortUrl = builder.shortUrl;
-//     }
-
-//     // Getters...
-
-//     public static Builder builder() {
-//         return new Builder();
-//     }
-
-
-    
-//     public static AliasWithUrlMapping getStubbedAliasWithUrlMappingForTesting() {
-//         AliasWithUrlMapping aliasWithUrlMapping = new AliasWithUrlMapping();
-//         aliasWithUrlMapping.setAlias("some-testing-alias");
-//         aliasWithUrlMapping.setFullUrl("https://example.com/very/long/url");
-//         aliasWithUrlMapping.setShortUrl("http://localhost:8080/my-custom-alias");
-
-//         return aliasWithUrlMapping;
-//     }
-
-// }
