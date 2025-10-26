c6face915da3790f5d34a42635546e2de77d8f22
Sean James <aseanjames2020@gmail.com>
2025-10-26 11:04:41 +0000

    Fix UrlShortenControllerTest, now we have the implementation this Test is no longer required run-ft-branch-cicd

--- src/test/java/com/subzero/tpximpact_challenge/controllers/UrlShortenControllerTest.java
+++ src/test/java/com/subzero/tpximpact_challenge/controllers/UrlShortenControllerTest.java
@@ -88,15 +88,6 @@ public class UrlShortenControllerTest {
     }
 
     @Test
-    void shortenUrlRequestPostEndpointShouldReturnBadRequestGivenNoImplementationCurrently() throws Exception {
-        mockMvc.perform(post("/api/v1/url-shortener/shorten")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{customAlias: \"my-alias\", fullUrl: \"http://example.com/some/long/url\"}"))
-                .andExpect(status().isBadRequest())
-                .andExpect(content().string("Invalid input or alias already taken"));
-    }
-
-    @Test
     public void shortenUrlRequestPostEndpointShouldReturnBadRequestIfExceptionFromService() throws Exception {
         AliasWithUrlMapping testAliasWithUrlMapping = MockAliasUrlMappingBuilder.getStubbedAliasWithUrlMappingForTesting();
 
